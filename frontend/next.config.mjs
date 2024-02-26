1161d8bc10174f3ffa089afe6f426fff0040b603
Dmitry Alekseev <Ezzy-Way@yandex.ru>
2024-02-26 23:36:40 +0400

    added: frontend/strapi/nginx/certbot docker-compose for production

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env : {
+    API_CONTAINER_URL: process.env.API_CONTAINER_URL,
+    API_CLIENT_URL: process.env.API_CLIENT_URL,
+    API_TOKEN: process.env.API_TOKEN,
+  },
+};
 
 export default nextConfig;
